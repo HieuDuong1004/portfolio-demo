75049d8a63db723c90aa131a4ddc21eb143b2fc1
HieuDuong <leecomchoe1@gmail.com>
2023-04-13 16:45:25 +0700

    style: edit width page

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,7 +1,7 @@
 // scroll to top 
 const scrollTop = document.querySelector('.scroll-top');
 window.addEventListener('scroll', function () {
-    if (window.scrollY > 250) {
+    if (window.scrollY > 300) {
         scrollTop.style.display = 'block';
     } else {
         scrollTop.style.display = 'none';
